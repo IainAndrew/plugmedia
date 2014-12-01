be9cb6260b3d1c0b1885a67b047be02d1545183b
Iain Andrew <iainbruceandrew@gmail.com>
2014-12-01 21:57:21 +0000

    updated version of footerreveal.js

--- app/scripts/footerreveal.js
+++ app/scripts/footerreveal.js
@@ -1,18 +1,28 @@
+/**
+ * footer-reveal.js
+ * 
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2014 Iain Andrew
+ * https://github.com/IainAndrew
+ */
+
 (function($) {
+
 	$.fn.footerReveal = function(options) {
+
 		var $this = $(this),
-	    $prev = $this.prev(),
+		    $prev = $this.prev(),
+		    $win = $(window),
+
 		defaults = $.extend ({
             shadow : true,
+            shadowOpacity: 0.8,
             zIndex : -100
         }, options );
-	 	
-	 	$this.css({
-		   position : 'fixed',
-		   bottom : 0,
-		});
 
-		if (defaults.zIndex) {
+        if (defaults.zIndex) {
 			$this.css({
 				'z-index' : defaults.zIndex
 			});
@@ -22,22 +32,45 @@
 			});
 		}
 
-		if ($prev.outerWidth !== $(window).width()) {
-			$this.css({
-				'max-width' : $prev.outerWidth()
-			});
-		}
-
-		$prev.css({
-			'margin-bottom' : $this.outerHeight()
+        $this.css({
+		   position : 'fixed',
+		   bottom : 0,
 		});
 
 		if (defaults.shadow) {
-			$prev.css({
-				'box-shadow' : '0 20px 30px -20px rgba(0,0,0,0.8)',
-				'-moz-box-shadow' : '0 20px 30px -20px rgba(0,0,0,0.8)',
-				'-webkit-box-shadow' : '0 20px 30px -20px rgba(0,0,0,0.8)'
-			});
+			if (defaults.shadowOpacity) {
+				$prev.css({
+					'-moz-box-shadow' : '0 20px 30px -20px rgba(0,0,0,' + defaults.shadowOpacity + ')',
+					'-webkit-box-shadow' : '0 20px 30px -20px rgba(0,0,0,' + defaults.shadowOpacity +')',
+					'box-shadow' : '0 20px 30px -20px rgba(0,0,0,' + defaults.shadowOpacity + ')'
+				});
+			} else {
+				$prev.css({
+					'-moz-box-shadow' : '0 20px 30px -20px rgba(0,0,0,' + options.shadowOpacity + ')',
+					'-webkit-box-shadow' : '0 20px 30px -20px rgba(0,0,0,' + options.shadowOpacity + ')',
+					'box-shadow' : '0 20px 30px -20px rgba(0,0,0,' + options.shadowOpacity + ')'
+				});
+			}
 		}
+
+		var footerRevealResize = function() {
+
+			$this.css({
+				'width' : $prev.outerWidth()
+			});
+
+			$prev.css({
+				'margin-bottom' : $this.outerHeight()
+			});
+		};
+
+		$win.load(function() {
+		    footerRevealResize();
+		});
+		$win.resize(function() {
+		    footerRevealResize();
+		});
+
 	}
+
 }) (jQuery);
